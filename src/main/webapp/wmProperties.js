c235f28f9fc5eb91ea2f1c1276b48fc0b945d8f0
Vishnu Alla <vishnu.alla@wavemaker.com>
2017-07-28 09:31:41 +0000

    wr New files added - [fonts,retro] Files modified - [data,webapp,.wmproject.properties] Removed files - [fonts,vermillion]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,5 +1,5 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "vermillion",
+  "activeTheme" : "retro",
   "defaultLanguage" : "en",
   "displayName" : "SalesVision_juLy2017",
   "homePage" : "Main",
